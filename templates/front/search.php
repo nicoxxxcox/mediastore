af033d2388c9c9013f7937e0975b014d9d66d9d2
nicolas.grissi <nicolas.grissi@edenwork.com>
2019-01-03 14:58:53 +0100

    <fix> Implement a message when search result is empty

--- templates/front/search.php
+++ templates/front/search.php
@@ -2,71 +2,50 @@
 require "./templates/front/_nav.php";
 ?>
 <div class="container py-5">
-
     <div class="row">
         <?php
         require "_search.php";
         ?>
     </div>
-
-
-
     <div class="row">
         <div class="col">
             <?php
-
-
-
-            echo " <p class=\"h2 my-5\">Resultat de votre recherche</p>" ;
-
-
+            echo " <p class=\"h2 my-5\">Resultat de votre recherche</p>";
             ?>
-
         </div>
     </div>
-
     <div class="row">
-
         <?php
         $res = $prod->searchProduct($_GET["s"]);
 
+        $search = $res->fetchAll(PDO::FETCH_ASSOC);
+       if(empty($search)){
+           echo "<p class=\" text-danger\">Aucuns resultats</p>";
+       }
+        foreach ($search as $row) {
 
-
-        while ($search = $res->fetch(PDO::FETCH_ASSOC)) {
-
+            extract($row);
             echo "
                 <div class=\"col-sm-3 my-4\">
                 <div class=\"card text-center\">
                     <div class=\"card-header\">
-                       ".$search["author_product"]."
+                       " . $author_product . "
                     </div>
-                    <img class=\"card-img-top\" src='".$search["url_product"]."' width=\"253\" height=\"151\" alt=\"Card image cap\" style=\"object-fit: cover\">
+                    <img class=\"card-img-top\" src='" . $url_product . "' width=\"253\" height=\"151\" alt=\"Card image cap\" style=\"object-fit: cover\">
                     <div class=\"card-body\">
-                    <h5 class=\"card-title \">".$search["price_product"]." €</h5>
-                        <h5 class=\"card-title text-warning\">".$search["name_product"]."</h5>
+                    <h5 class=\"card-title \">" . $price_product . " €</h5>
+                        <h5 class=\"card-title text-warning\">" . $name_product . "</h5>
 
-                        <a href=\"?page=product&product=".$search["id_product"]."&categorie=".$search["categorie_product"]."\" class=\"btn btn-primary\">Voir produit</a>
+                        <a href=\"?page=product&product=" . $id_product . "&categorie=" . $categorie_product . "\" class=\"btn btn-primary\">Voir produit</a>
                     </div>
                     <div class=\"card-footer text-muted\">
-                        <div class=\"btn btn-sm btn-light \">Ajouté le ".$search["dateadd"]."</div>
+                        <div class=\"btn btn-sm btn-light \">Ajouté le " . $dateadd . "</div>
                     </div>
                 </div>
-            </div>
-            
-            
-            ";
+            </div>";
         }
-
         ?>
-
-
-
-
-
     </div>
-
-
-
 </div>
 
 
