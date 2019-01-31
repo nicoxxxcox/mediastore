bf1b831e71df3ff2d8dafac4eb2c881b1d062824
nicolas.grissi <nicolas.grissi@edenwork.com>
2019-01-31 11:52:02 +0100

    Correction de la largeur de la cellule quantité dans le tableau panier

--- templates/front/orders.php
+++ templates/front/orders.php
@@ -10,15 +10,15 @@ if (!isset($subtotal)) {
 <div class="row">
     <div class="container py-5">
         <p class="h2">Mon panier</p>
-        <table class="table table-striped mt-5">
+        <table class="table table-striped mt-5" >
             <thead>
             <tr>
-                <th class="text-center" scope="col">Produit id</th>
+                <th class="text-center" scope="col">id</th>
                 <th class="text-center" scope="col">Article</th>
                 <th class="text-center" scope="col">Auteur</th>
                 <th class="text-center" scope="col">Catégorie</th>
                 <th class="text-center">prix</th>
-                <th class="text-center" scope="col">Stock</th>
+                <th class="text-center" scope="col">En stock</th>
                 <th class="text-center" scope="col">Livraison prévue</th>
                 <th class="text-center">Quantité</th>
                 <th class="text-center">TOTAL</th>
@@ -37,10 +37,15 @@ if (!isset($subtotal)) {
                         <td class="text-center"> - </td>
                         <td class="text-center"> - </td>
                         <td class="text-center"> - </td>
-                        <td class="text-center">
+                        <td class="text-center" >
                             <form action="">
-                                <input type="number" name="quantite" value=" - " id="">
-                                <button type="button" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></button>
+                            <div class="input-group">
+                                <input type="number" name="quantite" class="form-control" value=" - " id="">
+                                <div class="input-group-append">
+                                    <button type="button" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></button>
+                                </div>
+                            
+                            </div>                              
                             </form>
                         </td>
                         <td class="text-center"> -  €</td>
@@ -60,16 +65,22 @@ if (!isset($subtotal)) {
                             <td class="text-center">
                                 <a href="?page=product&categorie=' . $c['categorie_product'] . '&product=' . $id . '">' . tronque($c['name_product'], 15) . '</a>
                             </td>
-                            <td class="text-center">' . tronque($c['author_product'], 15) . '</td>
+                            <td class="text-center">' . tronque($c['author_product'], 30) . '</td>
                             <td class="text-center">' . whatCategory($c['categorie_product']) . '</td>
                             <td class="text-center">' . $c['price_product'] . ' €</td>
                             <td class="text-center">' . $c['state_product'] . '</td>
                             <td class="text-center">??</td>
-                            <td class="text-center">
-                                <form >
-                                    <input type="text"  name="quantite" value="' . $value . '" >
+                            <td class="text-center" style="table-layout: fixed;width: 10%;">
+                            <form action="">
+                            <div class="input-group ">
+                                <input type="text"  name="quantite" class="form-control text-center " value="' . $value . '" >
+                                <div class="input-group-append">
                                     <button type="button" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></button>
-                                </form>
+                                </div>
+                            
+                            </div>                              
+                            </form>
+                            
                             </td>
                             <td class="text-center">' . $c['price_product'] * $value . ' €</td>
                         </tr>';
@@ -83,13 +94,13 @@ if (!isset($subtotal)) {
             </tbody>
         </table>
 
-        <div class="card mb-3 text-center bg-warning">
+        <div class="card mb-5 text-center bg-warning">
             <div class="card-body">
                <h4 class="font-weight-bold"> Total de votre commande : <?php echo $subtotal ?> €</h4>
             </div>
         </div>
 
-        <div class="btn btn-primary "><a href="index.php?page=payment" class="text-light">Payer et passer la
+        <div class="btn btn-primary "><a href="index.php?page=payment" class="text-light"><i class="far fa-credit-card"></i>&nbsp;&nbsp;Payer et passer la
                 commande</a></div>
 
 
